5bfa72c86cfc40029830b1ba8bacf051acc9521b
JORGEMATIASDH <jorge.dominguezh@uanl.edu.mx>
2023-11-18 21:20:57 -0600

    Practica 14
    Generar Spiner, Instalación de librerías para generar capa android/capa ios

---
+++ ionic-basic/ionic-basic/src/app/app-routing.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AutGuardGuard } from './service/aut-guard.guard';
+
+const routes: Routes = [
+  {
+    path: 'logout',
+    loadChildren: () => import('./logout/logout.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: '/main/receta',
+    pathMatch: 'full'
+  },
+  {
+    path: 'main',
+    children:[
+      {
+        path: 'presupuesto',
+        loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
+      },
+      {
+        path: 'destinos',
+        loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
+      },
+      {
+        path: 'receta',
+        loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule)
+      },
+      {
+        path: 'detalle-receta',
+        loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule)
+      },
+      {
+        path: 'tabs',
+        loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+      },
+      {
+        path: 'alumnos',
+        loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+      },
+      {
+        path: 'destinos-api',
+        loadChildren: () => import('./destinos-api/destinos-api.module').then( m => m.DestinosApiPageModule)
+      },
+      {
+        path: 'galeria',
+        loadChildren: () => import('./galeria/galeria.module').then( m => m.GaleriaPageModule)
+      }
+    ],
+    canActivate: [AutGuardGuard]
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
